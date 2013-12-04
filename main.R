53636725822e103c78d304c833ae99055171d568
khodya <dukk@bk.ru>
2013-12-04 09:21:17 +0400

    downloadDay function without zip

--- main.R
+++ main.R
@@ -11,16 +11,29 @@ year <- "2002"
 # Example link: ftp://cddis.gsfc.nasa.gov/gps/data/daily/2002/175/02n/brdc1750.02n.Z
 # Example link 2: ftp://cddis.gsfc.nasa.gov//pub/gps/data/daily/2000/brdc/brdc0010.00n.Z
 
-base_link <- paste0(
-  "ftp://cddis.gsfc.nasa.gov/gps/data/daily/",
-  year,
-  day,
-  shortYear,
-  "n/"
-)
 
+# This function will download daily GPS data
+# for given day in a year and will store it
+# on disk as a data frame.
+# Input parameters: year, day
+# Output: file on disk, data frame
 
-downloadFile <- function(year,day)
+downloadDay <- function(year,day)
 {
+  short_year <- substr(year, 3, 4)
   
+  zipLink <- paste0(
+    "ftp://cddis.gsfc.nasa.gov//pub/gps/data/daily/",
+    year,
+    "/brdc/brdc",
+    day,
+    "0.",
+    short_year,
+    "n.Z"
+  )
+  
+  zippedData <- readLines(zipLink)
+  data <- unzip(zippedData)
 }
+
+fr <- downloadDay(2002,175)
